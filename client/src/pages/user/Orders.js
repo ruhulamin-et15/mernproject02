66aaa1ae2a44bafebfdc2ea4cb091224586620d0
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2023-11-17 01:29:31 +0600

    user orders add

--- client/src/pages/user/Orders.js
+++ client/src/pages/user/Orders.js
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Layout from "./../../components/layout/Layout";
+import UserMenu from "../../components/layout/UserMenu";
+import axios from "axios";
+import { useAuth } from "../../context/auth";
+import moment from "moment";
 
 const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
+  const [orders, setOrders] = useState([]);
+  const [auth, setAuth] = useAuth();
 
-export default Orders
+  const getOrders = async () => {
+    try {
+      const { data } = await axios.get("/api/auth/orders");
+      setOrders(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (auth?.token) getOrders();
+  }, [auth?.token]);
+  return (
+    <Layout>
+      <div className="container-fluid m-2 p-2">
+        <div className="row">
+          <div className="col-sm-4">
+            <UserMenu />
+          </div>
+          <div className="col-sm-8">
+            <h2 className="text-center">All Orders</h2>
+            {orders?.map((o, i) => {
+              return (
+                <div className="border shadow">
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <td scope="col">#</td>
+                        <td scope="col">Status</td>
+                        <td scope="col">Buyer</td>
+                        <td scope="col">Date</td>
+                        <td scope="col">Payment</td>
+                        <td scope="col">Quantity</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <th>{i + 1}</th>
+                        <th>{o?.status}</th>
+                        <th>{o?.buyer?.name}</th>
+                        <th>{moment(o?.createAt).fromNow()}</th>
+                        <th>{o?.payment.success ? "Failed" : "Success"}</th>
+                        <th>{o?.products?.length}</th>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <div className="container">
+                    {o?.products?.map((p, i) => (
+                      <div className="row mb-2 p-3 card flex-row">
+                        <div className="col-md-4">
+                          <img
+                            src={`/api/product/get-photo/${p._id}`}
+                            className="card-img-top"
+                            alt={p.name}
+                            width="100px"
+                            height={"100px"}
+                          />
+                        </div>
+                        <div className="col-md-5">
+                          <p>Name: {p.name}</p>
+                          <p>Desc: {p.description.substring(0, 30)}</p>
+                          <p>Price: {p.price}</p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Orders;
