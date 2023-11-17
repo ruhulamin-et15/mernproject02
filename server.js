49b312400178963d190cd912c17116a4282192c7
Ruhul Amin <150836543+ruhulamin-et15@users.noreply.github.com>
2023-11-17 13:16:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoute.js";
 import productRoutes from "./routes/productRoute.js";
 import path from "path";
+import {fileURLToPath} from "url";
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
